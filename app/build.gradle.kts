a24f68a8fc403f31eedd0f8d697c53d732954f02
sagarannaldas <annal7057@gmail.com>
2024-01-20 10:50:52 +0530

    1. Setting up Material3 Theme and Type-Safe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,8 @@ android {
 }
 
 dependencies {
+    implementation(projects.theme)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,5 +14,7 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "ModernDiaryApp"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
- 
+include(":theme")
